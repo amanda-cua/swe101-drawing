ef344358172ba45fad0dbeec2efec1aca2feec84
amanda-dc <68808193+amanda-dc@users.noreply.github.com>
2020-08-29 13:56:08 +0800

    triangle

--- script.js
+++ script.js
@@ -1,20 +1,21 @@
 // Please declare functions and variables above where they are used.
 var main = function (input) {
   var myOutputValue = '';
-
   var lineCounter = 0;
+  var emojiCounter = 1;
 
   while (lineCounter < input) {
     var columnCounter = 0;
   
-    while (columnCounter < input) {
+    while (columnCounter < emojiCounter) {
       myOutputValue = myOutputValue + '☺️';
-
       columnCounter = columnCounter + 1;
+  
     }
 
+    // start a new line
     myOutputValue = myOutputValue + '<br>';
-  
+    emojiCounter = emojiCounter + 1;
     lineCounter = lineCounter + 1;
   }
 
