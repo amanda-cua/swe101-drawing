b488d264a30d91fed637ecdc9b9bbd5f697f4f50
amanda-dc <68808193+amanda-dc@users.noreply.github.com>
2020-08-29 14:56:24 +0800

    added game modes

--- script.js
+++ script.js
@@ -1,23 +1,59 @@
 // Please declare functions and variables above where they are used.
-var main = function (input) {
-  var myOutputValue = '';
+
+  var myOutputValue;
   var lineCounter = 0;
-  var emojiCounter = 1;
+  var counter = 0;
+  var currentGameMode = 'waiting for game mode';
+  console.log ('waitingforgamemode')
 
-  while (lineCounter < input) {
-    var columnCounter = 0;
-  
-    while (columnCounter < emojiCounter) {
-      myOutputValue = myOutputValue + '☺️';
-      columnCounter = columnCounter + 1;
-  
-    }
-
-    // start a new line
-    myOutputValue = myOutputValue + '<br>';
-    emojiCounter = emojiCounter + 1;
-    lineCounter = lineCounter + 1;
+var main = function (input) {
+//selecting game mode
+  if (currentGameMode == 'waiting for game mode') {
+    currentGameMode = input;
+    myOutputValue = "Hello.  You have selected " + input + " game mode. <br>";
+    console.log ('select')
   }
 
+  else if (currentGameMode == 'numbers') {
+      while (counter < input) {
+        myOutputValue = myOutputValue + '☺️' ;
+        counter = counter + 1;}
+        console.log ('numberss')
+  }
+
+  else if (currentGameMode == 'square') {
+    while (lineCounter < input) {
+      var columnCounter = 0;
+      while (columnCounter < input) {
+        myOutputValue = myOutputValue + '☺️';
+        columnCounter = columnCounter + 1;
+      }
+      myOutputValue = myOutputValue + '<br>';
+      lineCounter = lineCounter + 1;
+      console.log ('squaress')
+  }
+}
+
+  else if  (currentGameMode == 'triangle') {
+    while (lineCounter < input) {
+      var columnCounter = 0;
+      while (columnCounter <= lineCounter) {
+        myOutputValue = myOutputValue + '☺️';
+        columnCounter = columnCounter + 1;
+      }
+      // start a new line
+      myOutputValue = myOutputValue + '<br>';
+      lineCounter = lineCounter + 1;
+      console.log ('triangless')
+    }
+    return myOutputValue;
+    }
+
+
+
   return myOutputValue;
-};
+}
+
+
+
+
