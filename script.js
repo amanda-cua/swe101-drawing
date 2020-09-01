c8117d6cd2635637a7a8f05c8b53856cd4814b26
amanda-dc <68808193+amanda-dc@users.noreply.github.com>
2020-09-01 19:49:15 +0800

    updates to drawing project

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Please declare functions and variables above where they are used.
+
 
   var myOutputValue;
   var lineCounter = 0;
@@ -10,49 +10,71 @@ var main = function (input) {
 //selecting game mode
   if (currentGameMode == 'waiting for game mode') {
     currentGameMode = input;
-    myOutputValue = "Hello.  You have selected " + input + " game mode. <br>";
-    console.log ('select')
+    myOutputValue = 'You have chosen the ' + input + ' game mode.<br> Have fun! <br>';
   }
-
+  //emoji numbers game mode (line of emojis based on input)
   else if (currentGameMode == 'numbers') {
       while (counter < input) {
         myOutputValue = myOutputValue + '☺️' ;
         counter = counter + 1;}
         console.log ('numberss')
   }
-
-  else if (currentGameMode == 'square') {
-    while (lineCounter < input) {
-      var columnCounter = 0;
-      while (columnCounter < input) {
-        myOutputValue = myOutputValue + '☺️';
-        columnCounter = columnCounter + 1;
-      }
-      myOutputValue = myOutputValue + '<br>';
-      lineCounter = lineCounter + 1;
-      console.log ('squaress')
-  }
+ //square game mode (squares are based on the dimensions of the input)
+ // i messed something up pls dont select the squares mode
+ else if (currentGameMode == 'square') {
+  while (lineCounter < input) {
+    var columnCounter = 0;
+    while (columnCounter < input) {
+      myOutputValue = myOutputValue + '☺️';
+      columnCounter = columnCounter + 1;
+    }
+    myOutputValue = myOutputValue + '<br>';
+    lineCounter = lineCounter + 1;
+    console.log ('squaress')
 }
-
+}
+//original triangle
   else if  (currentGameMode == 'triangle') {
-    while (lineCounter < input) {
+    while (columnCounter < input) {
       var columnCounter = 0;
       while (columnCounter <= lineCounter) {
         myOutputValue = myOutputValue + '☺️';
-        columnCounter = columnCounter + 1;
+        lineCounter = lineCounter + 1;
       }
       // start a new line
       myOutputValue = myOutputValue + '<br>';
-      lineCounter = lineCounter + 1;
+      columnCounter = columnCounter + 1;
       console.log ('triangless')
     }
     return myOutputValue;
     }
 
+  //coding attempt for upside down triangle
 
-
+  else if (currentGameMode == 'upside down') {
+    var myOutputValue = " ";
+var main = function (input) {
+ var lineCounter = input;
+ var overallTriangle = " ";
+ while (lineCounter > 0) {
+   var line = " ";
+   var columnCounter = lineCounter;
+   while (columnCounter > 0) {
+     line = line + "☺️";
+     columnCounter--;
+   }
+   overallTriangle = overallTriangle + line + "<br>"
+   lineCounter--;
+ }
+ return overallTriangle
+ 
+  }
+  
+}
   return myOutputValue;
 }
+
+ 
 
 
 
